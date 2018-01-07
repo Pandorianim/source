d29744c0dd7f5bdaafe958bcb9aee7268d0f3c98
Pandorianim <34489349+Pandorianim@users.noreply.github.com>
2018-01-07 21:51:46 +0100

    Zmiana linków

--- basicBot.js
+++ basicBot.js
@@ -260,8 +260,8 @@
         settings: {
             botName: 'basicBot',
             language: 'english',
-            chatLink: 'https://rawgit.com/basicBot/source/master/lang/en.json',
-            scriptLink: 'https://rawgit.com/basicBot/source/master/basicBot.js',
+            chatLink: 'https://rawgit.com/Pandorianim/source/master/lang/en.json',
+            scriptLink: 'https://rawgit.com/Pandorianim/source/master/basicBot.js',
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
