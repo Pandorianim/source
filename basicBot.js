86852831d7525996c0ba28f6976b1d6ab371a8c2
Pandorianim <34489349+Pandorianim@users.noreply.github.com>
2018-03-24 17:35:22 +0100

    powrot

--- basicBot.js
+++ basicBot.js
@@ -3,8 +3,6 @@
  Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
  This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
  */
-var lang = basicBot.userUtilities.getUser(user).language;
-if(lang=='pl')
 {
 (function() {
 
@@ -4273,4 +4271,4 @@ if(lang=='pl')
     };
 
     loadChat(basicBot.startup);
-}).call(this);}
+}).call(this);
