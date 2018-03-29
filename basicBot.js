cd7806eac8898a3871610cdf4c437cf3b5dbd4eb
Pandorianim <34489349+Pandorianim@users.noreply.github.com>
2018-03-29 14:40:26 +0200

    Dodanie !xd

--- basicBot.js
+++ basicBot.js
@@ -3233,6 +3233,21 @@
                 }
             },
 
+
+            xdCommand: {
+                command: ['xd', 'XD', 'xD'],
+                rank: 'user',
+                type: 'exact',
+                functionality: function(chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void(0);
+                    else {
+                        API.sendChat(basicBot.chat.xd)
+                    }
+                }
+            },
+
+
             sayingCommand: {
                 command: 'saying',
                 rank: 'user',
