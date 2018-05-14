8d202512b9a8e7a8d99a04a99b8e4d6febdb8cc0
Pandorianim <34489349+Pandorianim@users.noreply.github.com>
2018-05-14 23:01:35 +0200

    Na boolean test

--- basicBot.js
+++ basicBot.js
@@ -3368,7 +3368,7 @@
                         var name = msg.substr(cmd.length + 2);
                         var user = basicBot.userUtilities.lookupUserName(name);
                         if (msg.length > cmd.length + 2) {
-                            if (!API.getUser(user)) {
+                            if (typeof user !== 'boolean') {
                              API.sendChat(subChat(basicBot.chat.summonUsed, {
                                     name: chat.un,
                                     name2: name
